92562a67d84a28560059f4d5be7f49e26cbb1d3c
Hackme-311 <148679592+Hackme-311@users.noreply.github.com>
2025-11-17 12:23:19 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,11 +20,6 @@ export default function Navbar() {
 
         {/* ---- Contact Section ---- */}
         <div className="contact_section">
-          {/* <a href="mailto:info@vidyashram.com" className="contact_item">
-            <FontAwesomeIcon icon={faMessage} className="icon" />
-            info@vidyashram.com
-          </a> */}
-
           <a href="tel:+917340065179" className="contact_item">
             <FontAwesomeIcon icon={faPhone} className="icon" />
             +91 734 006 5179
